ae5fa0a9004711425a5a70a192d200c13f15ba56
Alan-Miller <alan.ashman@gmail.com>
2017-08-03 00:43:34 -0600

    up through step 4

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import "./index.css";
-
 import store from "./store";
-
 import App from "./components/App";
 
 ReactDOM.render(
-	<Provider store={ store }>
-		<App />
-	</Provider>,
+	<Router>
+		<Provider store={ store }>
+			<App />
+		</Provider>
+	</Router>
+	,
 	document.getElementById( "root" )
 );
